36ef24a70457fc1b592859f62b34ff58bedfc836
Michael Toth <michael@virtualpianist.com>
2024-11-17 13:07:23 -0500

    no errors

---
+++ vendor/javascript/a.js
@@ -0,0 +1,4 @@
+// a@3.0.1 downloaded from https://ga.jspm.io/npm:a@3.0.1/index.js
+
+import*as e from"a_mock";var a="default"in e?e.default:e;var r={};(function avoid_caching(){delete{}[new URL(import.meta.url).pathname]})();var t=a;r={mock:t.mock,expectRequire:t.expectRequire,requireMock:t.requireMock,then:t.then};var o=r;const c=r.mock;export{o as default,c as mock};
+
