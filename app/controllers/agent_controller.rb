0705cfb54c128feebb348bb7888d772a455556fb
Michael Toth <michael@virtualpianist.com>
2024-11-18 07:06:06 -0500

    fixed some formatting added paper background

--- app/controllers/agent_controller.rb
+++ app/controllers/agent_controller.rb
@@ -2,6 +2,9 @@ class AgentController < ApplicationController
   def home
   end
 
+  def about
+  end
+
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :concerts
   get 'agent/home'
   get 'agent/index'
+  get 'agent/about'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
