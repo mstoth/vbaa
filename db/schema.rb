f669936df5a70f1706792533608763cb11525290
Michael Toth <michael@virtualpianist.com>
2024-11-18 06:18:55 -0500

    fixed some formatting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_17_185211) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_18_094904) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_17_185211) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "group_id"
+    t.integer "venue_id"
+    t.integer "concert_id"
+    t.datetime "date_booked"
+    t.decimal "booked_fee"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "concerts", force: :cascade do |t|
